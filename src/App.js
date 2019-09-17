ce179bf16ff8be21ee5a8b2e9001b63eaacdc4f2
ElzaCS <anuejohn@gmail.com>
2019-09-17 18:17:23 +0530

    feat: import project card and navbar with sidebar (for mobile screens)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import CustomizedDialogs from './components/projectcard'
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+      <CustomizedDialogs />
 		</div>
 	);
 }
