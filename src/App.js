02418e94d18ccc35f8c448ccd7a74ca537c9c18e
raglandcodes <raglandasir@gmail.com>
2019-10-11 18:23:52 +0530

    setup relay

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import './App.css';
 import LandingPage from './views/LandingPage/LandingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Projects from './Projects';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <Projects style={{ color: 'pink' }} />
+      {/* <div className="App-header">
         <LandingPage />
-      </div>
+      </div> */}
     </div>
   );
 }
